c0b6e8a10603708c272917cacc00b5df5a5fc583
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-02 17:45:41 -0800

    Test 1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var text = 'Five Nights at Fazbois ';
+ctx.font = '30px Courier New';
+ctx.fillStyle = "#ffffff"
+ctx.fillText(text,10,35);
