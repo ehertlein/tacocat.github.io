599fe1faa5cbcb49706fc87b9b5f128b032229d6
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-02 17:49:26 -0800

    fixing image to draw from canvas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
+
+const image = document.getElementById('title');
+tx.drawImage(image, 0, 0);
+
 var text = 'Five Nights at Fazbois ';
 ctx.font = '30px Courier New';
+
 ctx.fillStyle = "#ffffff"
 ctx.fillText(text,10,35);
