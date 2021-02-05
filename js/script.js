52398d9a9c6e2f792a930715fbe4175b2f34aa0e
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-04 17:34:36 -0800

    Updates the order to try to ensure loading

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ var ctx = canvas.getContext('2d');
 
 let url = '../images/fazboiTitle.jpg';
 let img = new Image();
-img.src = url;
 img.onload = function () {
     ctx.drawImage(img, 0, 0);
 }
+img.src = url;
+
 // const image = document.getElementById('title');
 // ctx.drawImage(img, 0, 0);
 
