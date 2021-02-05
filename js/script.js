2a98215170b2a122822252af676d9aa1ddbfba40
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-04 17:23:32 -0800

    Updates canvas size and image loading

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+let url = '../image/fazboiTitle.jpg';
+let img = new Image();
+await new Promise(r => img.onload=r, img.src=url);
+
 const image = document.getElementById('title');
-ctx.drawImage(image, 0, 0);
+ctx.drawImage(img, 0, 0);
 
 var text = 'Five Nights at Fazbois ';
 ctx.font = '30px Courier New';
