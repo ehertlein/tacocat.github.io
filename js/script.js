d937b5b16b75b9b129af5698eab2a91bcf4e7065
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-04 17:57:04 -0800

    Another attempt at loading images

--- js/script.js
+++ js/script.js
@@ -1,29 +1,13 @@
-function imageLoaded(image) {
-    return new Promise(
-        function (resolve, reject) {
-            image.addEventListener("load", function() { resolve(image) });
-        }
-    );
+function drawTitle() {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    
+    const image = document.getElementById('title');
+    ctx.drawImage(img, 0, 0);
+    
+    var text = 'Five Nights at Fazbois ';
+    ctx.font = '30px Courier New';
+    
+    ctx.fillStyle = "#ffffff"
+    ctx.fillText(text,10,35);
 }
-
-async function getImage(url) {
-    var image = new Image();
-    image.src = url;
-    await imageLoaded(image);
-    return image;
-}
-
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-
-let image = getImage("images/fazboiTitle.jpg");
-ctx.drawImage(image, 0, 0);
-
-// const image = document.getElementById('title');
-// ctx.drawImage(img, 0, 0);
-
-var text = 'Five Nights at Fazbois ';
-ctx.font = '30px Courier New';
-
-ctx.fillStyle = "#ffffff"
-ctx.fillText(text,10,35);
