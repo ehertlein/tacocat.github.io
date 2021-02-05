61d5710c670eb8480a2f725e1aa6c00a455a3bd9
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-04 17:26:21 -0800

    Updates image loading code

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var ctx = canvas.getContext('2d');
 
 let url = '../image/fazboiTitle.jpg';
 let img = new Image();
-await new Promise(r => img.onload=r, img.src=url);
-
-const image = document.getElementById('title');
-ctx.drawImage(img, 0, 0);
+img.src = url;
+img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+}
+// const image = document.getElementById('title');
+// ctx.drawImage(img, 0, 0);
 
 var text = 'Five Nights at Fazbois ';
 ctx.font = '30px Courier New';
