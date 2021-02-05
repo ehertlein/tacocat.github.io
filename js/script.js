0104168ec641d351c46834668f45212304da9e5f
Eric Hertlein <eric.hertlein@gmail.com>
2021-02-04 17:45:38 -0800

    Using a promise to wait for the loading

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+function imageLoaded(image) {
+    return new Promise(
+        function (resolve, reject) {
+            image.addEventListener("load", function() { resolve(image) });
+        }
+    );
+}
+
+async function getImage(url) {
+    var image = new Image();
+    image.src = url;
+    await imageLoaded(image);
+    return image;
+}
+
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-let url = 'images/fazboiTitle.jpg';
-let img = new Image();
-img.onload = function () {
-    ctx.drawImage(img, 0, 0);
-}
-img.src = url;
+let image = getImage("images/fazboiTitle.jpg");
+ctx.drawImage(image, 0, 0);
 
 // const image = document.getElementById('title');
 // ctx.drawImage(img, 0, 0);
